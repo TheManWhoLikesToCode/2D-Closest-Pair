9d4dcc6171e8c93bd7571ef6b2341f61136115e9
Jaydin <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2022-11-28 19:46:56 -0500

    Fixin er

---
+++ src/Solver2DClosestPair.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class Solver2DClosestPair {
+
+    // plot points
+    public static void main(String[] args) {
+
+        long startTime, endTime, duration;
+        int pointsToTest = 5000000;
+        int trials = 5;
+        int[] fastAlgoTime = new int[trials];
+        int[] slowAlgoTime = new int[trials];
+
+        for (int i = 0; i < trials; i++) {
+
+            points[] P = points.generatePoints(pointsToTest);
+            points[] Q = P.clone();
+            Arrays.sort(P, points::compareX);
+            Arrays.sort(Q, points::compareY);
+
+            // Start time
+            startTime = System.nanoTime();
+            int[] fastAlgoPoints = points.efficientClosetPair(P, Q);
+            // End time
+            endTime = System.nanoTime();
+            // Duration
+            duration = (endTime - startTime) / 1000000;
+
+            // Add duration to array
+            fastAlgoTime[i] = (int) duration;
+
+            // Start time
+            startTime = System.nanoTime();
+            //int[] bruteForcePoints = points.exhaustiveSearch(P);
+            // End time
+            endTime = System.nanoTime();
+            // Duration
+            duration = (endTime - startTime) / 1000000;
+
+            // Add duration to array
+            slowAlgoTime[i] = (int) duration;
+
+        }
+
+        // Print results
+        System.out.println("Fast Algorithm: " + Arrays.toString(fastAlgoTime));
+        System.out.println("Slow Algorithm: " + Arrays.toString(slowAlgoTime));
+
+        // Calculate average
+        int fastAlgoAverage = 0;
+        int slowAlgoAverage = 0;
+        
+        for (int i = 0; i < trials; i++) {
+            fastAlgoAverage += fastAlgoTime[i];
+            slowAlgoAverage += slowAlgoTime[i];
+        }
+
+        fastAlgoAverage /= trials;
+        slowAlgoAverage /= trials;
+
+        System.out.println("Fast Algorithm Average: " + fastAlgoAverage);
+        System.out.println("Slow Algorithm Average: " + slowAlgoAverage);
+
+    }
+}
---
+++ src/points.java
@@ -0,0 +1,157 @@
+import java.util.Arrays;
+
+public class points {
+
+    // X and Y points
+    public int x;
+    public int y;
+
+    // Constructor
+    public points(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // Algebraic distance formula
+    public static float dist(points p1, points p2) {
+        return (float) Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
+    }
+
+    public static int compareY(points p1, points p2) {
+        // Compare y points
+        return Integer.compare(p1.getY(), p2.getY());
+    }
+
+    public static int compareX(points p1, points p2) {
+        // Compare x points
+        return Integer.compare(p1.getX(), p2.getX());
+    }
+
+    // generate a distict set of points
+    public static points[] generatePoints(int n) {
+        points[] points = new points[n];
+
+        for (int i = 0; i < n; i++) {
+            points[i] = new points((int) (Math.random() * 10000), (int) (Math.random() * 10000));
+        }
+
+        return points;
+    }
+
+    // split array method
+    public static points[] splitArray(points[] points, int start, int end) {
+        // Split array into two halves
+        points[] split = new points[end - start];
+        for (int i = start; i < end; i++) {
+            split[i - start] = points[i];
+        }
+        return split;
+    }
+
+    // exhaustive search method
+    public static int[] exhaustiveSearch(points[] points) {
+        int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+        // Initialize minimum distance to infinity
+        float min = Float.POSITIVE_INFINITY;
+        // Loop through all points
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                // Calculate distance between points
+                float dist = dist(points[i], points[j]);
+                // Update minimum distance
+                if (dist < min) {
+                    min = dist;
+                    // X and Y coordinates of closest points
+                    x1 = points[i].getX();
+                    y1 = points[i].getY();
+                    x2 = points[j].getX();
+                    y2 = points[j].getY();
+                }
+            }
+        }
+        return new int[] { x1, y1, x2, y2, (int) min };
+    }
+
+    public static int[] efficientClosetPair(points[] P, points[] Q) {
+
+        if (P.length <= 3) {
+            return exhaustiveSearch(P);
+        }
+
+        // Split array into two halves
+        points[] P1 = splitArray(P, 0, P.length / 2);
+        points[] P2 = splitArray(P, P.length / 2, P.length);
+        points[] Q1 = splitArray(Q, 0, Q.length / 2);
+        points[] Q2 = splitArray(Q, Q.length / 2, Q.length);
+
+        // Find closest pair in each half
+        int[] dLeft = efficientClosetPair(P1, Q1);
+        int[] dRight = efficientClosetPair(P2, Q2);
+
+        // Find minimum distance
+        int[] d = dLeft[4] < dRight[4] ? dLeft : dRight;
+
+        // Get the x coordinate of the middle point
+        int m = P[P.length / 2].getX();
+
+        // Find points in strip
+        points[] strip = new points[Q.length - 1];
+
+        // Copy all the points of Q into strip for which abs(x - m) < d
+        int ponitsOnSterip = 0;
+        int j = 0;
+        for (int i = 0; i < Q.length - 1; i++) {
+            if (Math.abs(Q[i].getX() - m) < d[4]) {
+                strip[j] = Q[i];
+                ponitsOnSterip++;
+                j++;
+            }
+        }
+
+        // Set dminsq equal to the square of the minimum distance
+        int dminsq = d[4] * d[4];
+
+        for (int i = 0; i < strip.length - 2; i++) {
+            int k = i + 1;
+            while (k <= ponitsOnSterip - 1 && Math.pow((strip[k].getY() - strip[i].getY()), 2) < dminsq) {
+                dminsq = Math.min(dminsq, (int) (Math.pow((strip[k].getX() - strip[i].getX()), 2)
+                        + Math.pow((strip[k].getY() - strip[i].getY()), 2)));
+                k++;
+            }
+        }
+
+        dminsq = (int) Math.sqrt(dminsq);
+
+        return new int[] { d[0], d[1], d[2], d[3], dminsq };
+        // return d;
+
+    }
+
+    // Method to do it all
+
+    public static int[] closestPairSolver(int n) {
+        points[] P = generatePoints(n);
+        points[] Q = P.clone();
+        Arrays.sort(P, points::compareX);
+        Arrays.sort(Q, points::compareY);
+        return efficientClosetPair(P, Q);
+    }
+
+    // Method to exhaustiveSearch
+    public static int[] exhaustiveSearchSolver(int n) {
+        points[] P = generatePoints(n);
+        points[] Q = P.clone();
+        Arrays.sort(P, points::compareX);
+        Arrays.sort(Q, points::compareY);
+        return exhaustiveSearch(P);
+    }
+}
